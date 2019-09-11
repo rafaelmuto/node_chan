8dd5dac475d6a3e60ade7b18c32cce59b5dbbbc8
Rafael Muto <r.nagahama@gmail.com>
2019-09-10 22:49:56 -0300

    why no gif??? =(

--- middlewares/uploadMiddleware.js
+++ middlewares/uploadMiddleware.js
@@ -14,7 +14,7 @@ const fileStorage = multer.diskStorage({
   }
 });
 const fileFilter = (req, file, cb) => {
-  if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+  if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg' || file.mimetype === 'image/gif') {
     cb(null, true);
   } else {
     cb(null, false);
